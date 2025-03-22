e9f92da881d55d86dbed545edb4db6ea59708061
Ramon Alves <ramon.alves160@gmail.com>
2025-03-22 18:36:14 -0300

    Adicionando os arquivos do projeto

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer id={styles.footer}>
+      <p>ReactGram &copy; 2025</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/pages/EditProfile/EditProfile.jsx
@@ -0,0 +1,145 @@
+import styles from "./EditProfile.module.css";
+
+import { uploads } from "../../utils/config";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+import { profile, resetMessage, updateProfile } from "../../slices/userSlice";
+
+// Components
+import Message from "../../components/Message/Message";
+
+const EditProfile = () => {
+  const dispatch = useDispatch();
+
+  const { user, message, error, loading } = useSelector((state) => state.user);
+
+  // States
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [bio, setBio] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  // Load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  // Fill form with user data
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setBio(user.bio);
+    }
+  }, [user]);
+
+  const handleFile = (e) => {
+    // image preview
+    const image = e.target.files[0];
+
+    setPreviewImage(image);
+
+    // update image state
+    setProfileImage(image);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Gather user data from states
+    const userData = {
+      name,
+    };
+
+    if (profileImage) {
+      userData.profileImage = profileImage;
+    }
+
+    if (bio) {
+      userData.bio = bio;
+    }
+
+    if (password) {
+      userData.password = password;
+    }
+
+    // build form data
+    const formData = new FormData();
+
+    const userFormData = Object.keys(userData).forEach((key) =>
+      formData.append(key, userData[key])
+    );
+
+    formData.append("user", userFormData)
+
+    await dispatch(updateProfile(formData))
+
+    setTimeout(() => {
+        dispatch(resetMessage())
+    }, 2000)
+  };
+
+  return (
+    <div id={styles.edit_profile}>
+      <h2>Edit Profile</h2>
+      <p className={styles.subtitle}>
+        Adicione uma imagem de perfil e conte mais sobre você...
+      </p>
+      {(user.profileImage || previewImage) && (
+        <img
+          className={styles.profile_image}
+          src={
+            previewImage
+              ? URL.createObjectURL(previewImage)
+              : `${uploads}/users/${user.profileImage}`
+          }
+          alt={user.name}
+        />
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input type="email" placeholder="E-mail" disabled value={email || ""} />
+        <label>
+          <span>Imagem do Perfil:</span>
+          <input type="file" onChange={handleFile} />
+        </label>
+        <label>
+          <span>Bio:</span>
+          <input
+            type="text"
+            placeholder="Descrição do perfil"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio || ""}
+          />
+        </label>
+
+        <label>
+          <span>Quer alterar sua senha ?</span>
+          <input
+            type="password"
+            placeholder="Digite sua nova senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ""}
+          />
+        </label>
+        {!loading && <input type="submit" value="Atualizar" />}
+        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="success" />}
+      </form>
+    </div>
+  );
+};
+
+export default EditProfile;
